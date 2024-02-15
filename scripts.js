7b671dce339d71faa0162067dddae870e9cf9dca
Brandon Taylor <156858984+brandontaylor1@users.noreply.github.com>
2024-02-14 23:19:55 -0500

    Improved functionality and bug fixes. Add source buttons, automate Top Headlines to load automatically

--- scripts.js
+++ scripts.js
@@ -1,58 +1,149 @@
-const { hrtime } = require("process");
-
 const API_KEY = process.env.NEWS_API_KEY
-const url = `https://newsapi.org/v2/everything?q=tesla&from=2024-01-14&sortBy=publishedAt&apiKey=${API_KEY}`
+const topHeadlinesURL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
 
-async function fetchNews() {
+let urlString = ""
+
+const input = document.getElementById('searchBar')
+const searchBtn = document.getElementById('searchBtn')
+searchBtn.addEventListener('click', () => {
+    console.log(input.value)
+})
+
+const buttons = document.getElementsByClassName('categoryBtn')
+const buttonPressed = event => {
+    urlString = event.target.id
+    console.log(urlString);
+}
+
+for(const button of buttons) {
+    button.addEventListener('click', event => {
+        buttonPressed(event);
+        fetchSources(urlString); 
+
+    })
+}
+
+async function fetchTopHeadlines() {
     try {
-        let response = await fetch(url)
-        let data = await response.json()
-        console.log(data);
-        displayNews(data.articles)
+        let response = await fetch(topHeadlinesURL);
+        let data = await response.json();
+        console.log(data)
+        displayArticles(data.articles)
 
-    } catch(error) {
-        console.error("Houston...", error )
+    } catch (error) {
+        console.error("Houston...", error) 
     }
 }
 
-fetchNews() 
+fetchTopHeadlines(); 
 
-
-function displayNews(articles) {
-    const newsDIV = document.getElementById('news')
+function displayArticles(articles) {
+    const articlesContainer = document.getElementById('articlesContainer');
+    
+    
     for (const article of articles) {
-
-        // const newCard = 
-        // ` <div class="card" style="width: 18rem;">
-        //     <img src=${article.urlToImage} class="card-img-top" alt="...">
-        //     <div class="card-body">
-        //         ${article.title}
-        //     <p class="card-text">${article.description}</p>
-        //     </div>
-        // </div>`
         
-        const articleDiv = document.createElement('div');
-        const title = document.createElement('h3');
-        const body = document.createElement('p')
-        const underline = document.createElement('hr')
-        const img = document.createElement('img')
-        const link = document.createElement('a')
-
+        const articlesDiv = document.createElement('div')
+        articlesDiv.setAttribute("id", 'articlesDiv')
         
-        img.src = article.urlToImage
-        img.width = 500;
+        
+        const title = document.createElement('h3')
+        title.setAttribute('id', 'title')
         title.textContent = article.title;
-        body.textContent = article.description;
-        link.href = url
-        
-        
-        articleDiv.appendChild(img)
-        articleDiv.appendChild(title);
-        articleDiv.appendChild(body);
-        articleDiv.appendChild(link);
-        articleDiv.appendChild(underline);
 
-        newsDIV.appendChild(articleDiv);
+        const body = document.createElement('p')
+        body.setAttribute('id','body')
+        body.textContent = article.description
         
+        
+        const newsImage = document.createElement('img')
+        newsImage.setAttribute('id','newsImage')
+        newsImage.src = article.urlToImage;
+        
+        const link = document.createElement('a')
+        link.setAttribute('id', 'articleLink')
+        link.href = article.url
+        
+        
+        if(!newsImage) {
+            newsImage.alt = "No Image Available"
+            continue
+        } else {
+            newsImage.src = article.urlToImage;
+        }
+        
+        
+        articlesDiv.appendChild(newsImage)
+        articlesDiv.appendChild(title);
+        articlesDiv.appendChild(body);
+        articlesDiv.appendChild(link);
+        
+        articlesContainer.appendChild(articlesDiv);
     }
+    
+}
+
+
+async function fetchSources() {
+    try {
+        let response = await fetch(`https://newsapi.org/v2/top-headlines/sources?category=${urlString}&apiKey=${API_KEY}`)
+        let data = await response.json()
+        console.log(data);
+        displaySources(data.sources)
+    
+    } catch(error) {
+        console.error("Houston...", error)
+        }
+    }
+
+
+function displaySources(sources) {
+    const articlesContainer = document.getElementById('articlesContainer');
+    
+    
+    for (const source of sources) {
+        
+        const articlesDiv = document.createElement('div')
+        articlesDiv.setAttribute("id", 'articlesDiv')
+
+
+        const title = document.createElement('h1')
+        title.setAttribute('id', 'title')
+        title.textContent = source.name;
+        
+        const body = document.createElement('p')
+        body.setAttribute('id','body')
+        body.textContent = source.description
+
+
+        const newsImage = document.createElement('img')
+        newsImage.setAttribute('id','newsImage')
+        newsImage.src = source.urlToImage;
+
+        const link = document.createElement('a')
+        link.setAttribute('id', 'articleLink')
+        link.href = source.url
+        
+        
+        if(!newsImage) {
+            newsImage.alt = "No Image Available"
+            continue
+        } else {
+            newsImage.src = source.urlToImage;
+        }
+
+        // articlesContainer.remove(articlesDiv); 
+                  
+        articlesDiv.appendChild(newsImage)
+        articlesDiv.appendChild(title);
+        articlesDiv.appendChild(body);
+        articlesDiv.appendChild(link);
+
+        articlesContainer.appendChild(articlesDiv);
+    }
+    
+}
+
+function clearCards() {
+    articlesContainer.re
 }
